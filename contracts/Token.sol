050c0e3208cdabe625b61176a168ed095db8cbda
IMPWONG <matisse.acheen.pro@gmail.com>
2021-03-02 17:03:59 -0500

    Constructor issue (to many argument or missing argument)

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,16 +1,34 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.6.0;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     address public minter;
+
+    //add minter changed event
+    event MinterChanged(address indexed from, address to);
     
-    constructor(
+   constructor(
         string memory name,
         string memory symbol,
         uint256 amount
-    ) public payable ERC20("ImpWong Currency", "IWC") {
+    ) public ERC20(name, symbol) {
         _mint(msg.sender, amount);
+    }
+
+    function passMinterRole(address dBank) public returns (bool) {
+        require(msg.sender==minter, 'Error, only owner can change pass minter role');
+        minter = dBank;
+
+        emit MinterChanged(msg.sender, dBank);
+        return true;
+    }
+
+    function mint(address account, uint256 amount) public {
+        //check if msg.sender have minter role
+        require(msg.sender == minter, 'Error, msg.sender does not have minter role'); 
+        _mint(account, amount);
     }
 }
